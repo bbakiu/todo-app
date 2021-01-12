29f8395e6d855af51c6f83ba6ee1152045a49a66
Bujar Bakiu <7114480+bbakiu@users.noreply.github.com>
2021-01-12 15:09:54 +0100

    fix urls

--- todobackend/src/todobackend/urls.py
+++ todobackend/src/todobackend/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include, re_path
-from todo import urls
+from django.urls import path, include
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    re_path('',  include('todo.urls')),
+    path('admin/', admin.site.urls),
+    path('',  include('todo.urls')),
 ]
