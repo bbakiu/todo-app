599a0893455d217fa9642261bd94ad0079fbd6d8
Bujar Bakiu <7114480+bbakiu@users.noreply.github.com>
2021-01-12 14:58:52 +0100

    Ädd views

--- todobackend/src/todo/views.py
+++ todobackend/src/todo/views.py
@@ -4,7 +4,6 @@ from todo.serializers import TodoItemSerializer
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.reverse import reverse
-from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 
--- todobackend/src/todobackend/urls.py
+++ todobackend/src/todobackend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, re_path
+from todo import urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('admin/', admin.site.urls),
+    re_path('',  include('todo.urls')),
 ]
